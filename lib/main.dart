21dcd136d7f192429786765ae4bd42f09b8e4a51
Nu Hla Phyu <nuhlaphyu@Nus-MacBook-Air.local>
2023-08-17 23:52:14 +0630

    add AppBar and home Background color

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
 
+//The main() function is the starting point for a 'Flutter' project
 void main() {
-  runApp(const MyApp());
-}
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.blueGrey[900],
+          title: Text("I Am Rich"),
+        ), // AppBar
 
+
+      ), // Scaffold
+    ), // MaterialApp
+  );
+}
