db53ea3c4a1217ba073b470b6ac2443c83a46537
Nu Hla Phyu <nuhlaphyu@Nus-MacBook-Air.local>
2023-08-18 12:17:49 +0630

    asset local image

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,14 @@ void main() {
           backgroundColor: Colors.blueGrey[900],
           title: Text("I Am Rich"),
         ), // AppBar
-        body: Center(
-          child: Image(
-            image: NetworkImage(
-                'https://miro.medium.com/max/4000/0*6WLqebrITTPNHwu7.gif'),
-            fit: BoxFit.fill,
-          ),
-          ),
+        // body: Center(
+        //   child: Image(
+        //     image: NetworkImage(
+        //         'https://miro.medium.com/max/4000/0*6WLqebrITTPNHwu7.gif'),
+        //     fit: BoxFit.fill,
+        //   ),
+        body: Image(image: AssetImage("assets/images/car.jpeg"),),
+
         // Center
 
       ), // Scaffold
